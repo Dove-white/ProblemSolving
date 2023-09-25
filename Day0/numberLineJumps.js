e3e25bad7e601a164d2a3808ed43a813728c6b71
Dove-White <dugbazahredeemer@gmail.com>
2023-09-25 22:49:41 +0000

    numberLineJump function

---
+++ Day0/numberLineJumps.js
@@ -0,0 +1,7 @@
+function kangaroo(x1, v1, x2, v2) {
+  // Write your code here
+  return v2 < v1 && (x2 - x1) % (v1 - v2) === 0 ? "YES" : "NO";
+}
+
+console.log(kangaroo(0, 3, 4, 2));
+console.log(kangaroo(0, 2, 5, 3));
